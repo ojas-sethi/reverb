122e6c5f14e5efe7ad6a2f7a547fef891075a35f
ojas-sethi <ojassethi2002@gmail.com>
2025-03-30 15:24:35 -0700

    Changes to routes

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const interSans = Inter({
+  variable: "--font-inter-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Reverb",
+  description: "An AI-powered platform for preparing for mock interviews.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body className={`${interSans.className} antialiased pattern`}>
+        {children}
+      </body>
+    </html>
+  );
+}
